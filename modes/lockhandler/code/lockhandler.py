ef28c84cb7cd599436c4ba9b2c809a12f01eea87
Anthony van Winkle <mpf@anthonyvanwinkle.com>
2018-02-15 17:05:26 -0800

    Mega reorg of global and field modes

--- modes/lockhandler/code/lockhandler.py
+++ modes/lockhandler/code/lockhandler.py
@@ -1,3 +1,4 @@
+import logging
 from mpf.core.mode import Mode
 
 class LockHandler(Mode):
@@ -29,13 +30,14 @@ class LockHandler(Mode):
 
   def __init__(self, machine, config, name, path):
     super().__init__(machine, config, name, path)
-    self.debug_log = self.machine.log.info
+    self.log = logging.getLogger("LockHandler")
+    self.log.setLevel("DEBUG")
     # We want to track whether to lock this ball, so when handling external events we can act accordingly
     self._will_lock_ball = False
 
   def mode_start(self, **kwargs):
     super().mode_start(**kwargs)
-    self.debug_log("LockHandler Mode is starting")
+    self.log.debug("LockHandler Mode is starting")
 
     # We need a pointer to the physical ball device to count physically locked balls
     for device in self.machine.ball_devices:
@@ -48,13 +50,13 @@ class LockHandler(Mode):
     # We need a shot that defines whether lock is lit
     try:
       lockshot = self.machine.shots.overlord_lock_ball_shot
-      self.debug_log("LockHandler is looking for overlord lock shot. " +
-        "state: {}, state_name: {}, enabled: {}".format(lockshot.state, lockshot.state_name,lockshot.enabled)
+      self.log.debug("LockHandler is looking for overlord lock shot. " +
+        "state: {}, state_name: {}, enabled: {}".format(lockshot.state, lockshot.state_name, lockshot.enabled)
         )
       if self.machine.shots.overlord_lock_ball_shot.enabled:
         self._post_event('enable_overlord_lock')
     except KeyError:
-      self.debug_log("LockHandler has no overlord lock shot, locking will be disabled")
+      self.log.debug("LockHandler has no overlord lock shot, locking will be disabled")
 
     self._register_handlers()
 
@@ -79,7 +81,7 @@ class LockHandler(Mode):
 
   def _handle_ball_enter(self, **kwargs):
     """ Logic for assessing desired behavior when a ball enters the physical ball lock device """
-    missions_available = self.machine.modes.recruitfield.active and self.player.available_missions > 0
+    missions_available = self.machine.modes.field.active and self.player.available_missions > 0
     physical_balls_locked = self._bd_physical_lock.balls
     virtual_balls_locked = self._overlordlock.locked_balls
     self._will_lock_ball = self._overlordlock.enabled
@@ -92,9 +94,9 @@ class LockHandler(Mode):
                       )
 
     if self._overlordlock.is_virtually_full:
-      self.debug_log(" - Lock is going to fill the multiball, skip lock handling")
+      self.log.debug(" - Lock is going to fill the multiball, skip lock handling")
     elif missions_available:
-      self.debug_log(" - Lock is not enabled but missions are available")
+      self.log.debug(" - Lock is not enabled but missions are available")
       self._post_event('start_mode_missionselect')
 
   def _handle_bypasscheck(self, **kwargs):
@@ -105,19 +107,19 @@ class LockHandler(Mode):
     # [AVW] Why no start multiball? Because I'm still trying to figure out why the balls_active count gets
     #       messed up and the multiball releases one less than it should. Ugh.
     if self._overlordlock.enabled and self._overlordlock.locked_balls < 2:
-      self.debug_log(" - Lock is lit, not going to bypass lock post")
+      self.log.debug(" - Lock is lit, not going to bypass lock post")
       return
 
     # HOLD:
-    # If no mission currently running (i.e. recruitfield is active) and a mission is available
+    # If no mission currently running (i.e. field mode is active) and a mission is available
     # (or if Garrus' mission is about to be, since the ball lock shot is also his mission light shot)
-    elif self.machine.modes.recruitfield.active and self.player.available_missions > 0 or self.player.status_garrus == 2:
-      self.debug_log(" - Recruitfield is active and missions are available, not going to bypass lock post")
+    elif self.machine.modes.field.active and self.player.available_missions > 0 or self.player.status_garrus == 2:
+      self.log.debug(" - Field mode is active and missions are available, not going to bypass lock post")
       return
 
     # BYPASS:
     # If neither of the above locking conditions, bypass the lock/hold
-    self.debug_log(" - Lock not enabled and no missions available, bypassing lock post")
+    self.log.debug(" - Lock not enabled and no missions available, bypassing lock post")
     self._bypass_lock()
 
   def _handle_missionselect_stop(self, **kwargs):
