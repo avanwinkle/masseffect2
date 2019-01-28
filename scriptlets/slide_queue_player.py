1ea7eda310c678aff0af069ed6bc9ea115cd20b8
Anthony van Winkle <mpf@anthonyvanwinkle.com>
2019-01-27 18:39:54 -0800

    Move slide_queue_player to mpf; enable flipper_cancel for action button

--- scriptlets/slide_queue_player.py
+++ scriptlets/slide_queue_player.py
@@ -1,12 +1,12 @@
 import logging
-from mpfmc.core.scriptlet import Scriptlet
+from mpf.core.custom_code import CustomCode
 from mpf.core.utility_functions import Util
 
 EXPIRE_SECS = 3
 EXPIRE_OVERLAP_SECS = 0.5
 DEFAULT_TRANS = { "type": "fade", "duration": 0.15 }
 
-class SlideQueuePlayer(Scriptlet):
+class SlideQueuePlayer(CustomCode):
   """
   This scriptlet creates a queue player for slides, allowing a series of slide
   play calls to be stacked and played sequentially with a given timeout.
@@ -18,8 +18,8 @@ class SlideQueuePlayer(Scriptlet):
     self._play_count = 0
     self._current_timeout = None
 
-    self.mc.events.add_handler("queue_slide", self._add_slide_to_queue)
-    self.mc.events.add_handler("check_slide_queue", self._check_queue_clear)
+    self.machine.events.add_handler("queue_slide", self._add_slide_to_queue)
+    self.machine.events.add_handler("check_slide_queue", self._check_queue_clear)
     self.log.info("slide Queue Player Ready!")
 
   def _add_slide_to_queue(self, **kwargs):
@@ -33,24 +33,16 @@ class SlideQueuePlayer(Scriptlet):
     del kwargs
     if self._queue:
       slide_name, slide_kwargs = self._queue.pop(0)
-      self.log.info("slide {} advance with kwargs {}".format(slide_name, slide_kwargs))
       expire = Util.string_to_secs(slide_kwargs.pop("expire", EXPIRE_SECS))
       timeout = expire - Util.string_to_secs(slide_kwargs.pop("expire_overlap", EXPIRE_OVERLAP_SECS))
       context = "global"
-      calling_context = None
+      calling_context = "queue_slide"
       settings = {
         slide_name: {
           "expire": expire,
           "action": "play",
-          # "key": slide_kwargs.get("key"),
           "target": slide_kwargs.get("target", None),
           "priority": 1000 + self._play_count,
-          # any of the below required?
-          # 'priority': None,
-          # 'background_color': [0.0, 0.0, 0.0, 1.0],
-          # 'show': True,
-          # 'force': False,
-          # 'slide': None,
           'transition': {
             "type": slide_kwargs.get("transition_type", DEFAULT_TRANS["type"]),
             "duration": slide_kwargs.get("transition_duration", DEFAULT_TRANS["duration"]),
@@ -67,9 +59,9 @@ class SlideQueuePlayer(Scriptlet):
         settings["portrait_slide"] = self._generate_portrait(expire, slide_kwargs)
         slide_kwargs["portrait_name"] = portrait_name
 
-      self.mc.slide_player.play(settings, context, calling_context, **slide_kwargs)
-      self.mc.post_mc_native_event("play_queued_slide_{}".format(slide_name), **slide_kwargs)
-      self._current_timeout = self.mc.clock.schedule_once(self._advance_queue, timeout)
+      self.machine.slide_player.play(settings=settings, context=context, calling_context=calling_context, **slide_kwargs)
+      self.machine.events.post("play_queued_slide_{}".format(slide_name), **slide_kwargs)
+      self._current_timeout = self.machine.clock.schedule_once(self._advance_queue, timeout)
       self._play_count += 1
 
     else:
@@ -96,4 +88,4 @@ class SlideQueuePlayer(Scriptlet):
 
   def _check_queue_clear(self, **kwargs):
     if not self._current_timeout:
-      self.mc.post_mc_native_event("slide_queue_clear")
+      self.machine.events.post("slide_queue_clear")
