82660740e584063a34d91fa03019bd32cefd7490
Anthony van Winkle <Anthony@Sephiroth.local>
2019-05-20 18:32:35 -0700

    Updates from playtest on Fast

--- modes/airlock/code/airlock.py
+++ modes/airlock/code/airlock.py
@@ -14,6 +14,9 @@ class Airlock(Mode):
         super().mode_start(**kwargs)
         self.log.info("Airlock Mode is starting")
         self.add_mode_event_handler("fmball_lightshot_hit", self._debug_lightshot_hit)
+        self.add_mode_event_handler("s_battering_ram_active", self._debug_enter)
+        ball_locks = self.machine.device_manager.get_monitorable_devices().get("multiball_locks")
+        self.lock = ball_locks["fmball_lock"]
 
     def _debug_lightshot_hit(self, **kwargs):
         self.log.info("FMBALL was hit, where we at?")
@@ -23,6 +26,11 @@ class Airlock(Mode):
         # self.log.info("Collections are {}".format(self.machine.device_manager.collections))
         # ball_lock_coll = self.machine.device_manager.collections["multiball_locks"]
         # self.log.info("ball lock collections are {}".format(ball_lock_coll))
-        fmball_lock = ball_locks["fmball_lock"]
-        self.log.info("FMBALL lock is {}".format(fmball_lock))
-        self.log.info(" - enabled? {}".format(fmball_lock.enabled))
+
+        self.log.info("FMBALL lock is {}".format(self.lock))
+        self.log.info(" - enabled? {}".format(self.lock.enabled))
+
+    def _debug_enter(self, **kwargs):
+        self.log.info("Ball has entered the airlock, lock is enabled? {}".format(self.lock.enabled))
+        self.log.info(" - lock has {} balls locked".format(self.lock.locked_balls))
+
