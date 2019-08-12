7d666c70612fb4814a463503ef995fdb612c320c
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2019-08-12 10:31:41 -0700

    Python 3.5 does not support ModuleNotFoundError

--- modes/pinstrat/code/pinstrat.py
+++ modes/pinstrat/code/pinstrat.py
@@ -1,14 +1,14 @@
 try:
-  from mpfpinstrat import PinStrat
-except ModuleNotFoundError:
-  from mpf.core.mode import Mode
+    from mpfpinstrat import PinStrat
+except ImportError:  # ModuleNotFoundError is only Python 3.6+
+    from mpf.core.mode import Mode
 
-  class PinStrat(Mode):
+    class PinStrat(Mode):
 
-    def __init__(self, machine, config, name, path):
-      super().__init__(machine, config, name, path)
-      machine.log.warning("MPF-PinStrat is not installed. PinStrat will not be run.")
-      machine.settings.set_setting_value("pinstrat_enabled", 0)
+        def __init__(self, machine, config, name, path):
+            super().__init__(machine, config, name, path)
+            machine.log.warning("MPF-PinStrat is not installed. PinStrat will not be run.")
+            machine.settings.set_setting_value("pinstrat_enabled", 0)
 
-    def mode_start(self, **kwargs):
-      self.stop()
+        def mode_start(self, **kwargs):
+            self.stop()
