d566e6a8dcefabf6688bf3347ea6809c276bc7aa
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2019-08-15 21:38:52 -0700

    Fix broken career loading

--- scriptlets/save_career.py
+++ scriptlets/save_career.py
@@ -28,15 +28,15 @@ class SaveCareer(CustomCode):
     def on_load(self):
         """Initialize the module by setting handlers for load/save-related events."""
         self.log = logging.getLogger("SaveCareer")
-        self.log.setLevel(10)
-        self._current_careers = {}
+        self.log.setLevel(20)
         self._achievement_handlers = {}
+        self._career_data = {}
         self._savepath = "{}/savegames".format(self.machine.machine_path)
         self.machine.events.add_handler("load_career", self._load_career)
         self.machine.events.add_handler("new_career", self._new_career)
         self.machine.events.add_handler("set_career", self._set_career)
         self.machine.events.add_handler("player_turn_will_end", self._save_career)
-        self.log.info("SaveCarrer loaded, what about squadmates?")
+        self.log.info("SaveCarrer loaded")
 
     def _set_career(self, **kwargs):
         if self.machine.game and self.machine.game.player:
@@ -108,94 +108,90 @@ class SaveCareer(CustomCode):
             os.makedirs(self._savepath, mode=0o755)
 
         self.log.debug("Saving career for '{}': {}".format(player.career_name, newcareer))
+        # Write a json file with the data
         json.dump(newcareer,
                   open(self._get_filename(player.career_name), mode="w"),
                   indent=2,
                   sort_keys=True
                   )
+        # Update the cached career data too
+        self._career_data[player.career_name] = newcareer
 
     def _load_career(self, **kwargs):
         player = self.machine.game.player
-        if self._current_careers.get(player.number):
-            career_name = kwargs.get("career_name",
-                                     self._current_careers[player.number]["career_name"])
+        career_name = kwargs.get("career_name")
+        setattr(player, "career_name", career_name)
 
-            setattr(player, "career_name", career_name)
+        if career_name == " ":
+            isDemo = self.machine.settings.demo_mode
+            CASUAL_CAREER = {
+                "career_name": " ",
+                "readonly": 1,
+                # Some mods for demo mode
+                "counter_sbdrops_counter": 2 if isDemo else 0,
+                "status_grunt": 3 if isDemo else 0,
+            }
+            careerdata = CASUAL_CAREER
+            setattr(player, "casual", 1)
+        else:
+            careerdata = self._fetch_careerdata(career_name)
+            setattr(player, "casual", 0)
 
-            if career_name == " ":
-                isDemo = self.machine.settings.demo_mode
-                CASUAL_CAREER = {
-                    "career_name": " ",
-                    "readonly": 1,
-                    # Some mods for demo mode
-                    "counter_sbdrops_counter": 2 if isDemo else 0,
-                    "status_grunt": 3 if isDemo else 0,
-                }
-                careerdata = CASUAL_CAREER
-                setattr(player, "casual", 1)
-            else:
-                careerdata = self._fetch_careerdata(career_name)
-                setattr(player, "casual", 0)
+        self._achievement_handlers = {}
+        available_missions = 0
+        squadmates_count = 0
 
-            self._achievement_handlers = {}
-            available_missions = 0
-            squadmates_count = 0
-
-            self.log.debug("Loading career {} for Player {} ====== Args={}".format(
-                           careerdata["career_name"], player.number, careerdata))
-
-            for key, value in careerdata.items():
-                if key.startswith("status_"):
-                    setattr(player, key, value)
-                    if value == 3:
-                        available_missions += 1
-                    elif value == 4:
-                        squadmates_count += 1
-                elif key in PLAYER_VARS or key.startswith("state_machine"):
-                    setattr(player, key, value)  # e.g. player.assignments_completed = 2
-                elif key == "achievements":
-                    for ach, state in careerdata["achievements"].items():
-                        handler = None
-                        # For achievements without "enable_events" set, listen for MPF to auto-enable the event
-                        if ach in ("overlord", "upgrade_armor", "upgrade_cannon", "upgrade_shields"):
-                            if state != "enabled":
-                                handler = self.machine.events.add_handler("achievement_{}_state_enabled".format(ach),
-                                                                          self._force_achievement,
-                                                                          achievement=ach,
-                                                                          state=state)
-                        # All other achievements will default to disabled, so listen for that to be set
-                        elif state != "disabled":
-                            handler = self.machine.events.add_handler("achievement_{}_state_disabled".format(ach),
+        for key, value in careerdata.items():
+            if key.startswith("status_"):
+                setattr(player, key, value)
+                if value == 3:
+                    available_missions += 1
+                elif value == 4:
+                    squadmates_count += 1
+            elif key in PLAYER_VARS or key.startswith("state_machine"):
+                setattr(player, key, value)  # e.g. player.assignments_completed = 2
+            elif key == "achievements":
+                for ach, state in careerdata["achievements"].items():
+                    handler = None
+                    # For achievements without "enable_events" set, listen for MPF to auto-enable the event
+                    if ach in ("overlord", "upgrade_armor", "upgrade_cannon", "upgrade_shields"):
+                        if state != "enabled":
+                            handler = self.machine.events.add_handler("achievement_{}_state_enabled".format(ach),
                                                                       self._force_achievement,
                                                                       achievement=ach,
                                                                       state=state)
-                        # Add a callback to remove the handler after we've set the achievement to the correct state
-                        if handler:
-                            self._achievement_handlers[ach] = handler
+                    # All other achievements will default to disabled, so listen for that to be set
+                    elif state != "disabled":
+                        handler = self.machine.events.add_handler("achievement_{}_state_disabled".format(ach),
+                                                                  self._force_achievement,
+                                                                  achievement=ach,
+                                                                  state=state)
+                    # Add a callback to remove the handler after we've set the achievement to the correct state
+                    if handler:
+                        self._achievement_handlers[ach] = handler
 
-                        # If this achievement is to be selected from the mission select, mark it as available
-                        if ach in ACHIEVEMENT_MISSIONS and state in ("enabled", "stopped"):
-                            available_missions += 1
+                    # If this achievement is to be selected from the mission select, mark it as available
+                    if ach in ACHIEVEMENT_MISSIONS and state in ("enabled", "stopped"):
+                        available_missions += 1
 
-            # Based on the squadmates and achievements, set the player's progress variables
-            setattr(player, "available_missions", available_missions)
-            setattr(player, "squadmates_count", squadmates_count)
+        # Based on the squadmates and achievements, set the player's progress variables
+        setattr(player, "available_missions", available_missions)
+        setattr(player, "squadmates_count", squadmates_count)
         # Allow the queue to continue
         self.machine.events.post("career_loaded", career_name=player.career_name)
 
     def _new_career(self, **kwargs):
         player = self.machine.game.player
-        if self._current_careers.get(player.number):
-            player.career_name = self._current_careers[player.number]["career_name"]
-            # All we have to do is set a new career_started time
-            player.career_started = datetime.now().timestamp()
-            # But also, transfer over the trophies if this profile existed
-            try:
-                careerdata = self._fetch_careerdata(player.career_name)
-                player.trophies = careerdata.get("trophies")
-            # If this career has never been saved, that's okay
-            except(FileNotFoundError):
-                pass
+        player.career_name = kwargs.get("career_name")
+        # All we have to do is set a new career_started time
+        player.career_started = datetime.now().timestamp()
+        # But also, transfer over the trophies if this profile existed
+        try:
+            careerdata = self._fetch_careerdata(player.career_name)
+            player.trophies = careerdata.get("trophies")
+        # If this career has never been saved, that's okay
+        except(FileNotFoundError):
+            pass
         self.machine.events.post("career_loaded", career_name=player.career_name)
 
     def _force_achievement(self, **kwargs):
@@ -214,7 +210,8 @@ class SaveCareer(CustomCode):
         return "{}/{}.json".format(self._savepath, career_name)
 
     def _fetch_careerdata(self, career_name):
-        with open(self._get_filename(career_name)) as f:
-            careerdata = json.load(f)
-        f.close()
-        return careerdata
+        if not self._career_data.get(career_name):
+            with open(self._get_filename(career_name)) as f:
+                self._career_data[career_name] = json.load(f)
+            f.close()
+        return self._career_data[career_name]
