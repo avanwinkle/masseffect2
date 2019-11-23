858e6de91f2bf36ad61c29fe880bf69c4c6ee219
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2019-11-22 17:36:34 -0800

    Update strategy doc for powers

--- modes/store/code/store.py
+++ modes/store/code/store.py
@@ -1,38 +1,38 @@
 
-# ** ARMOR UPGRADES **
-# These upgrades are aquired from the store and provide a
-# passive benefit during gameplay. Only one upgrade can be
-# equipped at a time, though previously-purchase upgrades can
-# be re-equipped at no cost. These upgrades provide more powerful
-# bonuses than the weapon upgrades. They do *not* persist
-# across games.
+# ** RESEARCH UPGRADES ** GOING TO TRY THIS
+# These upgrades are aquired from the store and provide
+# passive benefits during gameplay. They are stackable
+# and persist throughout the player's career.
+#
+# When the player enters a store, one of the four elements is chosen at random
+# and the two options for that element are available to purchase (or the player
+# can decline and purchase nothing).
+#
+# For the 1-6 minor upgrades, the player can purchase up to 1/2 of their current
+# level and can purchase the same minor upgrade once per level. If the player
+# cannot afford or has already purchased the minor upgrade, it will be grayed
+# out. If they cannot afford the major upgrade, it will be grayed out.
+#
+# Random selection of the store to open will be weighed against what's
+# available. Minerals with both grayed out will not be chosen. Minerals with one
+# available have 1/2 the chance of those with two. If there are no purchase
+# options, the store will not be enabled and the player can re-light the store
+# to try again.
 #
 #  - Shield upgrades
 #      - Damage protection 1-6: increase ball save times [PALLADIUM]
 #      - Redundant field generator: random chance for a ball save on drain [PALLADIUM]
 #  - Cybernetic upgrades:
 #      - Heavy Skin weave 1-6: increase points and multipliers [IRIDIUM]
-#  - Bio-amp upgrades:
-#      - Biotic cooldown: accelerate awarding of powers [EEZO]
-#      - Biotic duration 1-6 increase timer durations [EEZO]
-#  - Omni-tool upgrades:
-#      - Tech cooldown: accelerate the awarding of powers [EEZO]
+#      - Argus Scanner Array: accelerate awarding of minerals [IRIDIUM]
 #  - Medigel upgrades
-#     - Trauma module: light both outlanes for medigel save [PLATINUM]
 #     - Medigel capacity 1-6: award medigel when lower lanes completed [PLATINUM]
-#  - Advanced Mineral Scanner
-#     - Argus Scanner Array: accelerate awarding of minerals [IRIDIUM]
-#
-#  Armor slots:
-#    - Stimulator conduits:     increase timer durations      (increase storm speed 10%)
-#    - Capacitor chestplate:    award medigel on lower lanes  (reduce shield delay 10%)
-#    - Life support webbing:    light medigel on both lanes   (increase health 10%)
-#    - N7 shoulder guards:      increase points               (increase damage 3%)
-#    - Recon hood:              increase points               (increase damage 5%)
-#    - N7 greaves:              increase ball save times      (increase shields 3%)
-#    - Heavy damping gauntlets: increase ball save times      (increase shields 5%)
-#    - Archon visor:            accelerate awarding of powers (reduce cooldown 5%)
-#    - Death mask:              accelerate mineral collection (increase negotiation 10%)
+#     - Trauma module: light both outlanes for medigel save [PLATINUM]
+#  - Bio-amp / Omni-tool upgrades:
+#      - Biotic duration: 1-6 increase timer durations [EEZO]
+#      - Tech duration: 1-6 increase timer durations [EEZO]
+#      - Biotic cooldown: accelerate awarding of powers [EEZO]
+#      - Tech cooldown: accelerate awarding of powers [EEZO]
 
 import logging
 from mpf.core.mode import Mode
@@ -132,3 +132,25 @@ class Store(Mode):
         available_powers = [power for power in BONUS_POWERS if power.is_affordable(player)]
         self.log.debug(" - Armors available: {}".format(available_armors))
         self.log.debug(" - Powers available: {}".format(available_powers))
+
+
+# ** ARMOR UPGRADES ** NOT GOING TO TRY YET
+# These upgrades are aquired from the store and provide a
+# passive benefit during gameplay. Only one upgrade can be
+# equipped at a time, though previously-purchase upgrades can
+# be re-equipped at no cost. These upgrades provide more powerful
+# bonuses than the weapon upgrades. They do *not* persist
+# across games.
+#
+#
+#  Armor slots:
+#    - Stimulator conduits:     increase timer durations      (increase storm speed 10%)
+#    - Capacitor chestplate:    award medigel on lower lanes  (reduce shield delay 10%)
+#    - Life support webbing:    light medigel on both lanes   (increase health 10%)
+#    - N7 shoulder guards:      increase points               (increase damage 3%)
+#    - Recon hood:              increase points               (increase damage 5%)
+#    - N7 greaves:              increase ball save times      (increase shields 3%)
+#    - Heavy damping gauntlets: increase ball save times      (increase shields 5%)
+#    - Archon visor:            accelerate awarding of powers (reduce cooldown 5%)
+#    - Death mask:              accelerate mineral collection (increase negotiation 10%)
+#
