941a8087265e899dcc2727cf789e68be40f41b3b
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2019-11-25 13:22:43 -0800

    Carousel select for store mode, perks take effect

--- scriptlets/research.py
+++ scriptlets/research.py
@@ -1,37 +1,8 @@
 """MPF handlers for upgrade eligibility."""
 
 import logging
-import math
 import random
 from mpf.core.custom_code import CustomCode
-
-RESEARCH = {
-  "iridium": {
-    "minor": ResearchUpgrade("Heavy Skin Weave",
-                             "tick_interval", 0.05, "iridium", 30000),
-    "major": ResearchUpgrade("Argus Scanner Array",
-                             "mineral_rate", 0.1, "iridium", 90000),
-  },
-  "palladium": {
-    "minor": ResearchUpgrade("Damage Protection",
-                             "ball_save_period", 0.1, "palladium", 30000),
-    "major": ResearchUpgrade("Redundant Field Generator",
-                             "random_ball_save", 0.03, "palladium", 60000),
-  },
-  "platinum": {
-    "minor": ResearchUpgrade("Medigel Capacity",
-                             "award_medigel", 0.05, "platinum", 25000),
-    "major": ResearchUpgrade("Trauma Module",
-                             "double_medigel", 0.1, "platinum", 50000),
-  },
-  "eezo": {
-    "minor": ResearchUpgrade("Biotic Duration",
-                             "power_tick_interval", 0.1, "eezo", 2500),
-    "major": ResearchUpgrade("Biotic Cooldown",
-                             "cooldown_rate", 0.1, "eezo", 7500),
-  }
-}
-
 
 
 class Upgrade:
@@ -54,58 +25,91 @@ class ResearchUpgrade(Upgrade):
     def __repr__(self):
         return "<ArmorUpgrade.{}:{} '{}''>".format(self.perk, self.amount, self.name)
 
+    def to_kwargs(self, player, next_level=False):
+        level = player["research_{}_level".format(self.perk)] + (1 if next_level else 0)
+        return {
+            "title": self.name,
+            "full_title": "{} {}".format(self.name, level),
+            "description": self.description.format(int(self.amount * 100 * level)),
+            "mineral": self.mineral,
+            "cost": self.cost,
+            "level": level
+        }
+
     def check(self, player):
-      # If the player already purchased this research at this level, no good
-      if player["level"] == player["research_{}_last_player_level".format(self.perk)]:
-        return False
-      # If the player's level is too low to purchase the next level of research
-      if math.ceil(player["level"] / 2) >= player["research_{}_level".format(self.perk)]:
-        return False
-      # Can the player afford it?
-      return player["mineral_{}".format(self.mineral)] >= self.cost
+        next_level = player["research_{}_level".format(self.perk)] + 1
+        # If the player already purchased this research at this level, no good
+        if player["level"] == player["research_{}_last_player_level".format(self.perk)]:
+            return False
+        # If the player's level is too low to purchase the next level of research
+        if (next_level * 2) - 1 > player["level"]:
+            return False
+        # Can the player afford it?
+        return player["mineral_{}".format(self.mineral)] >= self.cost
 
     def award(self, player):
-      # Increase the player's perk by the research perk amount
-      player["research_{}_perk".format(self.perk)] += self.amount
-      # Track the level of this research project
-      player["research_{}_level".format(self.perk)] += 1
-      # Track the level at which the player purchased this research
-      player["research_{}_last_player_level".format(self.perk)] = player["level"]
+        # Increase the player's perk by the research perk amount
+        player["research_{}_perk".format(self.perk)] += self.amount
+        # Track the level of this research project
+        player["research_{}_level".format(self.perk)] += 1
+        # Track the level at which the player purchased this research
+        player["research_{}_last_player_level".format(self.perk)] = player["level"]
+        # And of course, pay for it
+        player["mineral_{}".format(self.mineral)] -= self.cost
 
 
-class BonusPower(Upgrade):
-    def __init__(self, name, description, power, mineral, cost):
-        super().__init__(name, description, mineral, cost)
-        self.power = power
-
-    def __repr__(self):
-        return "<BonusPower.{}>".format(self.name)
-
-    def activate(self, player):
-        player["power"] = self.power
-        player["power_is_bonus"] = 1
-
-BONUS_POWERS = [
-    BonusPower("Adrenaline Rush", "Stop all timers for 15s", "adrenaline", "eezo", 4000),
-    BonusPower("Tech Armor", "Enable 10s ball save", "armor", "eezo", 4000),
-    BonusPower("Tactical Cloak", "Use flippers to rotate lit shots", "cloak", "eezo", 8000),
-    BonusPower("Biotic Charge", "Hit a lit shot at random", "charge", "eezo", 8000),
-    BonusPower("Combat Drone", "Add a ball", "drone", "eezo", 6000),
-    BonusPower("Singularity", "Target hits count as lane hits", "singularity", "eezo", 10000),
-]
+RESEARCH = {
+    "tick_interval": ResearchUpgrade("Heavy Skin Weave", "Extend timers by {}%",
+                                     "tick_interval", 0.05, "iridium", 30000),
+    "mineral_rate": ResearchUpgrade("Argus Scanner Array", "Earn {}% more minerals",
+                                    "mineral_rate", 0.1, "iridium", 90000),
+    "ball_save_period": ResearchUpgrade("Damage Protection", "Increase ball saves by {}%",
+                                        "ball_save_period", 0.1, "palladium", 30000),
+    "random_ball_save": ResearchUpgrade("Redundant Field Generator", "{}% chance of ball save on drain",
+                                        "random_ball_save", 0.03, "palladium", 60000),
+    "award_medigel": ResearchUpgrade("Medigel Capacity", "Earn medigel {}% faster",
+                                     "award_medigel", 0.05, "platinum", 25000),
+    "double_medigel": ResearchUpgrade("Trauma Module", "{}% chance of double medigel",
+                                      "double_medigel", 0.1, "platinum", 50000),
+    "power_tick_interval": ResearchUpgrade("Biotic Duration", "Increase power duration by {}%",
+                                           "power_tick_interval", 0.1, "eezo", 2500),
+    "cooldown_rate": ResearchUpgrade("Biotic Cooldown", "Accelerate power cooldown by {}%",
+                                     "cooldown_rate", 0.1, "eezo", 7500),
+}
 
 
 class Research(CustomCode):
-  def on_load(self):
-    self.machine.events.add_handler("check_research", self._on_check)
+    def on_load(self):
+        self.log = logging.getLogger("Research")
+        self.log.setLevel(1)
+        self.log.debug("Research custom code started!")
+        self.machine.events.add_handler("check_research", self._on_check)
+        self.machine.events.add_handler("start_mode_store", self._on_store)
+        self.machine.events.add_handler("research_purchased", self._on_purchase)
 
-  def _on_check(self, **kwargs):
-    minerals = []
-    player = self.machine.game.player
-    for mineral, upgrades in RESEARCH.items():
-      # One weight for each
-      for upgrade in upgrades.values():
-        if upgrade.check(player):
-          minerals.append(mineral)
+    def get_purchaseable_options(self):
+        options = []
+        player = self.machine.game.player
+        for key, upgrade in RESEARCH.items():
+            if upgrade.check(player):
+                options.append(key)
+        return options
 
-    self.machine.events.post("research_check_{}".format("passed" if len(minerals) > 0 else "failed"))
+    def _on_check(self, **kwargs):
+        success = len(self.get_purchaseable_options()) > 0
+        self.machine.events.post("research_check_{}".format("passed" if success else "failed"))
+
+    def _on_purchase(self, **kwargs):
+        upgrade = RESEARCH[kwargs["selection"]]
+        player = self.machine.game.player
+        if not upgrade.check(player):
+            self.log.warning("Player attempted to purchase upgrade {} but check failed!".format(upgrade))
+            return
+        upgrade.award(player)
+        self.machine.events.post("upgrade_awarded", **upgrade.to_kwargs(player))
+
+    def _on_store(self, **kwargs):
+        options = self.get_purchaseable_options()
+        result = random.sample(options, k=min(len(options), 3))
+        self.machine.game.player["store_options"] = "|".join(result)
+        self.machine.events.post("research_options", options=result)
