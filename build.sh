e7643e7390f9f30b29e1d71720db729a030be9b6
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2020-07-19 18:37:20 -0700

    Comments for build script

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 # Script to build production config bundles for MPF. Includes the production.yaml
 # config file for embedded machines (production builds don't accept command-line
 # config arguments)
+echo "Building production bundle for Mass Effect 2 Pinball..."
 mpf build production_bundle -c config,production
--- prod.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Script for running production build (HARDWARE platform) of MPF on an embedded machine
-python -m mpf both ~/masseffect2 -t -P
