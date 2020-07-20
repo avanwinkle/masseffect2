03a3a022003061e3e348d86f25e5e4a4be47a5eb
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2020-07-19 17:11:12 -0700

    Fix machine setting value call

--- scriptlets/save_career.py
+++ scriptlets/save_career.py
@@ -49,7 +49,7 @@ class SaveCareer(CustomCode):
             if kwargs.get("casual"):
                 player["casual"] = 1
                 player["career_name"] = "Player {}".format(player.number)
-                player["high_flow"] = self.machine.settings.get_setting_value("casual_flow", 0)
+                player["high_flow"] = self.machine.settings.get_setting_value("casual_flow")
                 # Duplicate events, we may have already set this
                 if not player["mineral_iridium"] :
                     for mineral in ["iridium", "palladium", "platinum"]:
