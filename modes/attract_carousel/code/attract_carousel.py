2526175804c1d170c4223a4cfe7cffa34e5a1c12
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2023-09-10 16:12:24 -0700

    Custom carousel for attract credits slides

---
+++ modes/attract_carousel/code/attract_carousel.py
@@ -0,0 +1,22 @@
+"""Custom mode code for Attract carousel."""
+
+
+from mpf.modes.carousel.code.carousel import Carousel
+
+
+class AttractCarousel(Carousel):
+
+    def mode_start(self, **kwargs):
+        super().mode_start(**kwargs)
+        if self.machine.variables.get_machine_var("free_play"):
+            return
+        self.add_mode_event_handler("machine_var_credit_units", self._on_credits)
+        self.add_mode_event_handler("not_enough_credits", self._on_credits)
+
+    def _on_credits(self, **kwargs):
+        # Don't jump slide on game start
+        if self.stopping or not self.active:
+            return
+        # Assume credits slide is index 1
+        self._highlighted_item_index = 1
+        self._update_highlighted_item(None)
