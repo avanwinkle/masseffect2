096fcd75d635261288292053a55eb4a2e9fe8cb6
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2023-09-26 13:31:33 -0700

    VersionChecker custom code to ensure pt-me2-stable

---
+++ scriptlets/version_checker.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Paradigm Tilt
+"""Custom version checker for pt-sov-stable."""
+
+from mpf.core.custom_code import CustomCode
+from mpf._version import __version__ as mpfversion
+
+REQUIRED_MPF_VERSION = "0.56.0-dev20"
+
+
+class VersionChecker(CustomCode):
+
+    """Check the current MPF version to ensure it's compatible."""
+
+    def on_load(self):
+        """Verify the correct version of MPF."""
+        if mpfversion != REQUIRED_MPF_VERSION:
+            raise ValueError(f"Found MPF version {mpfversion}, but {REQUIRED_MPF_VERSION} is required.")
