9ba15c6dc4658dbef7cf4e83be1413f5463271f1
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2023-12-31 12:43:24 -0800

    Delay global stop and bonus start for slides

---
+++ modes/bonus/code/bonus.py
@@ -0,0 +1,21 @@
+"""Custom mode code for Global."""
+
+from mpf.modes.bonus.code.bonus import Bonus as BonusBase
+from custom_code.squadmates_mpf import SquadmateStatus
+
+class Bonus(BonusBase):
+
+    __slots__ = ("standups", "is_lock_slide_active")
+
+    def mode_start(self, **kwargs):
+        self.log.setLevel(10)
+        if self.machine.modes["global"].active:
+            self.log.debug("Bonus mode starting, waiting for global")
+            self.add_mode_event_handler("slide_queue_clear", self._on_proceed, **kwargs)
+        else:
+            self._on_proceed(**kwargs)
+
+    def _on_proceed(self, **kwargs):
+        self.log.debug("Bonus mode proceeding")
+        self.machine.events.post("bonus_queue_clear")
+        super().mode_start(**kwargs)
